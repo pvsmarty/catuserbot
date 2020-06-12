ee3093e95bd0aa95ca6fcf2b3b43313256e6a22f
pvsmarty <64521463+pvsmarty@users.noreply.github.com>
2020-06-12 15:36:43 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -149,7 +149,7 @@ async def on_afk(event):
         message_to_reply = f"__My Master Has Been Gone For__ `{total_afk_time}`\nWhere He Is: IN A PEACEFUL WORLD " + \
             f"\n\n__I promise I'll back in a few light years__\n**REASON**: {reason}" \
             if reason \
-            else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? Soon __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  " \
+            else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? Soon __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  " 
            else f"**hy...! its pv personal message**\n__Don't Disturb me i need some peace...see you soon sorry if i hurt u "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
