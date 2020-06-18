629590821a796dda7684d872fdd049a35c6461f8
pvsmarty <64521463+pvsmarty@users.noreply.github.com>
2020-06-18 09:49:20 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -19,16 +19,16 @@ from telethon.tl.types import InputStickerSetID
 from telethon.tl.types import DocumentAttributeSticker
 
 KANGING_STR = [
-    "Using Witchery to kang this sticker...😂",
+    "Using Witchery to kang this sticker...",
     "Plagiarising hehe...",
-    "Inviting this sticker over to my pack...🤘",
-    "Kanging this sticker...🤭",
-    "Hey that's a nice sticker!\nMind if I kang?!..🤪",
-    "hehe me stel ur stikér\nhehe.😒",
-    "Ay look over there (☉｡☉)!→\nWhile I kang this...😊",
+    "Inviting this sticker over to my pack...",
+    "Kanging this sticker...",
+    "Hey that's a nice sticker!\nMind if I kang?!..",
+    "hehe me stel ur stikér\nhehe.",
+    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
     "Roses are red violets are blue, kanging this sticker so my pacc looks cool🤗",
-    "Imprisoning this sticker...😚",
-    "Mr.Steal Your Sticker is stealing this sticker...😜 ",
+    "Imprisoning this sticker...",
+    "Mr.Steal Your Sticker is stealing this sticker... ",
 ]
 
 
